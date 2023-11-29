550b955e11a3ec8a581f448ca7c58197198eeeef
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-29 11:46:51 +0000

    update train.py

--- train.py
+++ train.py
@@ -161,6 +161,7 @@ while epoch < num_epochs + 1:
                 'steps_loss': steps_loss,
                 'epoch': epoch,
                 'epoch_loss': epoch_loss,
+                'batch_idx': batch_idx,
                 'lr': optimizer.param_groups[0]['lr'],
                 'model_state_dict': model.state_dict(),
                 'optimizer_state_dict': optimizer.state_dict(),
