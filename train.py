abb81bd025665d6c7fd8f4c636bc41ee22dd070f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-11-30 09:27:36 +0000

    update train.py

--- train.py
+++ train.py
@@ -137,7 +137,7 @@ while epoch < num_epochs + 1:
         rgb_before_blurred = F.interpolate(rgb_before_blurred, scale_factor = 64, mode='bilinear', align_corners=False)
 
         # loss = (rgb_output - rgb_after).abs().mean()
-        loss = criterion(rgb_output, rgb_after)
+        loss = criterion_l1(rgb_output, rgb_after)
         loss_l1 = criterion_l1(rgb_output, rgb_after)
         epoch_loss.append(float(loss_l1))
         steps_loss.append(float(loss_l1))
