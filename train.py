d3b2f8bbdef40a84304515beda2af709cc554d83
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-01 00:17:42 +0000

    update train.py

--- train.py
+++ train.py
@@ -194,7 +194,7 @@ while epoch < num_epochs + 1:
         hsl_loss = criterion_mse(rgb_to_hsl(rgb_output), rgb_to_hsl(rgb_after))
         yuv_loss = criterion_mse(rgb_to_yuv(rgb_output), rgb_to_yuv(rgb_after))
         rgb_loss = criterion_mse(rgb_output, rgb_after)
-        loss = 0.95 * rgb_loss + 0.05 * hsl_loss
+        loss = yuv_loss
         loss_l1 = criterion_l1(rgb_output, rgb_after)
         epoch_loss.append(float(loss_l1))
         steps_loss.append(float(loss_l1))
