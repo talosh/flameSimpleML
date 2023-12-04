1cfb568138881aa88c0281835e4591d84fdbd2c9
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-04 22:31:40 +0000

    update multiresnet.py

--- model/multiresnet.py
+++ model/multiresnet.py
@@ -308,4 +308,4 @@ class MultiResUnet(torch.nn.Module):
 		# print (f'\nmax: {torch.max(out):.4f}')
 		# print (f'min: {torch.min(out):.4f}')
 		
-		return torch.tanh(out)
+		return torch.tanh(out/2)
