2ccc085c951044218c9809f08e8be6a2cb267668
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-04 14:14:25 +0000

    update multiresnet.py and train.py

--- train.py
+++ train.py
@@ -177,7 +177,7 @@ while epoch < num_epochs + 1:
         for param_group in optimizer.param_groups:
             param_group['lr'] = current_lr
 
-        rgb_output = model(before)
+        rgb_output = model((before*2 -1))
 
         rgb_before = before[:, :3, :, :]
         rgb_after = after[:, :3, :, :]
