35b5b5f9cd5d53ae184e1f36d4338494878480ca
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-11 19:18:12 +0000

    update flameSimpleML_inference.py

---
+++ inference/flameSimpleML_inference.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+try:
+    from PySide6 import QtWidgets, QtCore, QtGui
+except ImportError:
+    from PySide2 import QtWidgets, QtCore, QtGui
+
+class flameSimpleMLInference(QtWidgets.QWidget):
+    def __init__():
+        pass
