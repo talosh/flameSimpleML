645058cbaedd972da563e50fb736ff966fa9f6f7
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-17 21:37:26 +0000

    update flameSimpleML.py

--- flameSimpleML.py
+++ flameSimpleML.py
@@ -16,7 +16,7 @@ settings = {
         'numpy>=1.16',
         'torch>=1.12.0'
     ],
-    'version': 'v0.0.1.dev.004',
+    'version': 'v0.0.1.dev.005',
 }
 
 def get_media_panel_custom_ui_actions():
