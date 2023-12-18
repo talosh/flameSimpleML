3dd58ab5c03fedef9c44558d9fa00f5fb76b2fca
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-18 20:55:28 +0000

    update threeplusnet.py

--- model/threeplusnet.py
+++ model/threeplusnet.py
@@ -753,12 +753,24 @@ class UNet_3PlusMemOpt(nn.Module):
         del h1_PT_hd3
         torch.cuda.empty_cache()
         
+        print ('h1_PT_hd3:')
+        allocated_memory = torch.cuda.memory_allocated(current_device)
+        reserved_memory = torch.cuda.memory_reserved(current_device)
+        print(f"Allocated memory: {allocated_memory / 1e9:.2f} GB")
+        print(f"Reserved memory:  {reserved_memory / 1e9:.2f} GB")
+
         h2 = h2_cpu.to(model_device)
         h2_PT_hd3 = self.h2_PT_hd3_relu(self.h2_PT_hd3_bn(self.h2_PT_hd3_conv(self.h2_PT_hd3(h2))))
         h2_PT_hd3_cpu = h2_PT_hd3.to('cpu')
         del h2
         del h2_PT_hd3
         torch.cuda.empty_cache()
+
+        print ('h2_PT_hd3:')
+        allocated_memory = torch.cuda.memory_allocated(current_device)
+        reserved_memory = torch.cuda.memory_reserved(current_device)
+        print(f"Allocated memory: {allocated_memory / 1e9:.2f} GB")
+        print(f"Reserved memory:  {reserved_memory / 1e9:.2f} GB")
 
         h3 = h3_cpu.to(model_device)
         h3_Cat_hd3 = self.h3_Cat_hd3_relu(self.h3_Cat_hd3_bn(self.h3_Cat_hd3_conv(h3)))
