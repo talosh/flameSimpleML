69f32b2310d0f660636acaf4dd4967e13948f0e9
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-18 19:28:18 +0000

    update threeplusnet.py

--- model/threeplusnet.py
+++ model/threeplusnet.py
@@ -342,7 +342,7 @@ class UNet_3Plus(nn.Module):
 class UNet_3PlusMemOpt(nn.Module):
 
     def __init__(self, in_channels=3, n_classes=1, feature_scale=4, is_deconv=True, is_batchnorm=True):
-        super(UNet_3Plus, self).__init__()
+        super().__init__()
         self.is_deconv = is_deconv
         self.in_channels = in_channels
         self.is_batchnorm = is_batchnorm
