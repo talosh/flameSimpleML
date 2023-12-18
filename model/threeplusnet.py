b62e9e5c81e09653463897e766f52ff7eef12c9e
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-18 21:06:21 +0000

    update threeplusnet.py

--- model/threeplusnet.py
+++ model/threeplusnet.py
@@ -899,9 +899,10 @@ class UNet_3PlusMemOpt(nn.Module):
         print(f"Reserved memory:  {reserved_memory / 1e9:.2f} GB")
 
         hd2_cat_cpu = torch.cat((h1_PT_hd2_cpu, h2_Cat_hd2_cpu, hd3_UT_hd2_cpu, hd4_UT_hd2_cpu, hd5_UT_hd2_cpu), 1)
+        print ('after cat cpu')
         hd2_cat = hd2_cat_cpu.to(model_device)
         print ('before conv')
-        
+
         hd2_conv = self.conv2d_1(hd2_cat)
 
         torch.cuda.empty_cache()
