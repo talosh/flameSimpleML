9617b968dd1928489c337b2eab23b6b59bf1e57b
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-18 21:10:01 +0000

    update threeplusnet.py

--- model/threeplusnet.py
+++ model/threeplusnet.py
@@ -886,9 +886,16 @@ class UNet_3PlusMemOpt(nn.Module):
         print(f"Reserved memory:  {reserved_memory / 1e9:.2f} GB")
 
         hd5 = hd5_cpu.to(model_device)
-        print('hd5 on gpu')
         hd5_ut = self.hd5_UT_hd2(hd5)
-        print('hd5_ut')
+        del hd5
+        torch.cuda.empty_cache()
+
+        print ('hd5_ut:')
+        allocated_memory = torch.cuda.memory_allocated(current_device)
+        reserved_memory = torch.cuda.memory_reserved(current_device)
+        print(f"Allocated memory: {allocated_memory / 1e9:.2f} GB")
+        print(f"Reserved memory:  {reserved_memory / 1e9:.2f} GB")
+
 
         hd5_UT_hd2 = self.hd5_UT_hd2_relu(self.hd5_UT_hd2_bn(self.hd5_UT_hd2_conv(self.hd5_UT_hd2(hd5))))
         hd5_UT_hd2_cpu = hd5_UT_hd2.to('cpu')
