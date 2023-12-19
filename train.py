c33785bb4efd4b38032272d2b13985eaf24cd381
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-19 21:37:05 +0000

    update train.py

--- train.py
+++ train.py
@@ -96,6 +96,7 @@ def save_images(save_img_queue):
         except queue.Empty:
             print ('hello')
             time.sleep(1)
+            continue
 
         sample_before = imgs[0].cpu().detach().numpy().transpose(1, 2, 0)
         cv2.imwrite('test/01_before.exr', sample_before[:, :, :3], [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_HALF])
