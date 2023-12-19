0f3ab5e3bbdbac883d988c8556f3b65b75e5ac0c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-19 21:34:37 +0000

    update train.py

--- train.py
+++ train.py
@@ -89,15 +89,26 @@ def rgb_to_yuv(rgb):
 
     return yuv
 
-def save_images(before, after, rgb_output):
-    sample_before = before.cpu().detach().numpy().transpose(1, 2, 0)
-    cv2.imwrite('test/01_before.exr', sample_before[:, :, :3], [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_HALF])
-    
-    sample_after = after.cpu().detach().numpy().transpose(1, 2, 0)
-    cv2.imwrite('test/02_after.exr', sample_after[:, :, :3], [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_HALF])
-    
-    sample_current = rgb_output.cpu().detach().numpy().transpose(1, 2, 0)
-    cv2.imwrite('test/03_output.exr', sample_current[:, :, :3], [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_HALF])
+def save_images(save_img_queue):
+    while True:
+        try:
+            imgs = save_img_queue.get_nowait()
+        except queue.Empty:
+            time.sleep(1e-4)
+
+        sample_before = imgs[0].cpu().detach().numpy().transpose(1, 2, 0)
+        cv2.imwrite('test/01_before.exr', sample_before[:, :, :3], [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_HALF])
+        
+        sample_after = imgs[1].cpu().detach().numpy().transpose(1, 2, 0)
+        cv2.imwrite('test/02_after.exr', sample_after[:, :, :3], [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_HALF])
+        
+        sample_current = imgs[2].cpu().detach().numpy().transpose(1, 2, 0)
+        cv2.imwrite('test/03_output.exr', sample_current[:, :, :3], [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_HALF])
+
+save_thread = threading.Thread(target=save_images, args=(save_img_queue))
+save_thread.daemon = True
+save_thread.start()
+
 
 log_path = 'train_log'
 num_epochs = 4444
@@ -248,8 +259,6 @@ while epoch < num_epochs + 1:
             except:
                 pass
             
-            # thread = threading.Thread(target=save_images, args=(before_clone, after_clone, rgb_output_clone))
-            # thread.start()
             '''
             sample_before = ((before[0].cpu().detach().numpy().transpose(1,2,0)))
             cv2.imwrite('test/01_before.exr', sample_before[:,:,:3], [cv2.IMWRITE_EXR_TYPE, cv2.IMWRITE_EXR_TYPE_HALF])
