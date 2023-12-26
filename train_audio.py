ebb73a556caa8d874f34ccacb00c943a59a35791
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-26 01:19:02 +0000

    update train_audio.py

--- train_audio.py
+++ train_audio.py
@@ -223,12 +223,12 @@ while epoch < num_epochs + 1:
         time_stamp = time.time()
         
         if step % 40 == 1:
-            sample_before = before[0][0].clone().cpu().detach().numpy()
-            sample_after = after[0][0].clone().cpu().detach().numpy()
-            sample_current = output[0][0].clone().cpu().detach().numpy()
-            output_audio_before = librosa.istft(sample_before, center=False)
-            output_audio_after = librosa.istft(sample_before, center=False)
-            output_audio_current = librosa.istft(sample_before, center=False)
+            sample_before = before[0][0].clone().cpu().detach().numpy()[:, 1:-1]
+            sample_after = after[0][0].clone().cpu().detach().numpy()[:, 1:-1]
+            sample_current = output[0][0].clone().cpu().detach().numpy()[:, 1:-1]
+            output_audio_before = librosa.istft(sample_before, n_fft=2047, hop_length=45, center=False)
+            output_audio_after = librosa.istft(sample_before, n_fft=2047, hop_length=45, center=False)
+            output_audio_current = librosa.istft(sample_before, n_fft=2047, hop_length=45, center=False)
             sf.write('test_audio/01_before.wav', output_audio_before, sr)
             sf.write('test_audio/02_after.wav', output_audio_after, sr)
             sf.write('test_audio/03_output.wav', output_audio_current, sr)
