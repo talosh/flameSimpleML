c6db4cc13e548555e743336693fdb301c1d0c16b
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2023-12-26 02:08:08 +0000

    update audio_identity.py

--- audio_identity.py
+++ audio_identity.py
@@ -32,14 +32,14 @@ processed_audio_segments = []
 for i, segment in enumerate(audio_segments):
     seg_spectrogram = librosa.stft(segment, n_fft=2047, hop_length=45,center=False)
 
-    seg_real = np.real(seg_spectrogram)
-    seg_imag = np.imag(seg_spectrogram)
-    two_channel_stft = np.stack((seg_real, seg_imag), axis=-1)
+    seg_mag = np.abs(seg_spectrogram)
+    seg_phase = np.angle(seg_spectrogram)
+    two_channel_stft = np.stack((seg_real, seg_imag), axis=0)
 
-    real_part = two_channel_stft[:, :, 0]
-    imaginary_part = two_channel_stft[:, :, 1]
+    mag = two_channel_stft[0]
+    phase = two_channel_stft[1]
 
-    stft_matrix_recombined = real_part + 1j * imaginary_part
+    stft_matrix_recombined = mag * np.exp(1j * phase)
     # print (f'max: {np.max(seg_spectrogram)}, min: {np.min(seg_spectrogram)}')
     # seg_spectrogram_db = librosa.amplitude_to_db(abs(seg_spectrogram))
 
