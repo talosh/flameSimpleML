e6929613f82e78a1fbc775b9104020022b6aeddf
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-01 15:07:47 +0000

    update flameSimpleML_inference.py

--- flameSimpleML_inference.py
+++ flameSimpleML_inference.py
@@ -1186,7 +1186,7 @@ class flameSimpleMLInference(QtWidgets.QWidget):
             import flame
             import numpy as np
 
-            pprint (selection)
+            pprint (dir(selection[0]))
 
             clip = selection[0]
             self.destination_node_name = clip.name.get_value() + '_ML'
