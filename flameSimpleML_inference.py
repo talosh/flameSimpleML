1ff9f72325d3e1cee6a2576c3d6fab3f35a5cc61
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-02 22:32:45 +0000

    update flameSimpleML_inference.py

--- flameSimpleML_inference.py
+++ flameSimpleML_inference.py
@@ -1688,6 +1688,13 @@ class flameSimpleMLInference(QtWidgets.QWidget):
                 self.rendering = False
                 self.frame_thread.join()
 
+    def stop_render_loop_thread(self):
+        if isinstance(self.frame_thread, threading.Thread):
+            if self.frame_thread.is_alive():
+                self.info(f'Frame {self.current_frame}: Stopping...')
+                self.rendering = False
+                self.frame_thread.join()
+
     def _process_current_frame(self, single_frame=False):
         
         import numpy as np
