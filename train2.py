6bb6f18bbf2747c39a8d61e89e6a49b31fbf6f89
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-02 11:56:07 +0000

    update train2.py

--- train2.py
+++ train2.py
@@ -18,7 +18,7 @@ import queue
 from model.accnet_w import ACC_UNet_W
 from model.accnet import ACC_UNet
 from model.accnet_lite import ACC_UNet_Lite
-from model.multires4_v001 import MultiResUnet
+from model.multires_v001 import MultiResUnet
 from model.threeplusnet import UNet_3Plus
 
 from dataset import myDataset
