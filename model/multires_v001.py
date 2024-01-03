871eb7df780f76aff52e71e8c60b9415c105a96b
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-03 05:37:39 +0000

    update multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -467,10 +467,12 @@ class MultiResUnet_MemOpt(torch.nn.Module):
 			try:
 				x_multires1 = self.multiresblock1(x)
 			except:
-				self.multiresblock1 = self.multiresblock1.to(device = 'cpu', dtype=torch.float32)
+				self.multiresblock1 = self.multiresblock1.cpu()
+				self.multiresblock1 = self.multiresblock1.to(dtype=torch.float32)
+				print ('hellp')
 				x_multires1 = self.multiresblock1(x.to(device = 'cpu', dtype=torch.float32))
 				del x
-				x_multires1 = x_multires1.to(device)
+				x_multires1 = x_multires1.to(device=device, dtype=torch.half)
 			print ('x_m1')
 			x_pool1 = self.pool1(x_multires1)
 			x_multires1 = self.respath1(x_multires1)
