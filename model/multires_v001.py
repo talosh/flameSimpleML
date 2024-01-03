e76b2afda5f4632864d0fd17067e55917491a29b
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-03 04:58:47 +0000

    update flameSimpleML_inference.py and multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -115,6 +115,51 @@ class Multiresblock(torch.nn.Module):
 	
 		return x
 
+class Multiresblock_MemOpt(torch.nn.Module):
+	'''
+	MultiRes Block
+	
+	Arguments:
+		num_in_channels {int} -- Number of channels coming into mutlires block
+		num_filters {int} -- Number of filters in a corrsponding UNet stage
+		alpha {float} -- alpha hyperparameter (default: 1.67)
+	
+	'''
+
+	def __init__(self, num_in_channels, num_filters, alpha=1.69):
+	
+		super().__init__()
+		self.alpha = alpha
+		self.W = num_filters * alpha
+		
+		filt_cnt_3x3 = int(self.W*0.167)
+		filt_cnt_5x5 = int(self.W*0.333)
+		filt_cnt_7x7 = int(self.W*0.5)
+		num_out_filters = filt_cnt_3x3 + filt_cnt_5x5 + filt_cnt_7x7
+		
+		self.shortcut = Conv2d_batchnorm(num_in_channels ,num_out_filters , kernel_size = (1,1), activation=False)
+
+		self.conv_3x3 = Conv2d_batchnorm(num_in_channels, filt_cnt_3x3, kernel_size = (3,3), activation=True, inplace=True)
+
+		self.conv_5x5 = Conv2d_batchnorm(filt_cnt_3x3, filt_cnt_5x5, kernel_size = (3,3), activation=True, inplace=True)
+		
+		self.conv_7x7 = Conv2d_batchnorm(filt_cnt_5x5, filt_cnt_7x7, kernel_size = (3,3), activation=True, inplace=True)
+
+		self.act = torch.nn.SELU(inplace=True)
+
+	def forward(self,x):
+
+		shrtct = self.shortcut(x)
+		
+		a = self.conv_3x3(x)
+		b = self.conv_5x5(a)
+		c = self.conv_7x7(b)
+
+		x = torch.cat([a,b,c],axis=1)
+		x = x + shrtct
+		x = self.act(x)
+	
+		return x
 
 class Respath(torch.nn.Module):
 	'''
@@ -141,17 +186,13 @@ class Respath(torch.nn.Module):
 			if(i==0):
 				self.shortcuts.append(Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (1,1), activation=False))
 				self.convs.append(Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (3,3),activation=True))
-
-				
 			else:
 				self.shortcuts.append(Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False))
 				self.convs.append(Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True))
 
 			self.bns.append(torch.nn.BatchNorm2d(num_out_filters))
 		
-	
 	def forward(self,x):
-
 		for i in range(self.respath_length):
 
 			shortcut = self.shortcuts[i](x)
@@ -166,6 +207,45 @@ class Respath(torch.nn.Module):
 
 		return x
 
+class Respath_MemOpt(torch.nn.Module):
+	'''
+	ResPath
+	
+	Arguments:
+		num_in_filters {int} -- Number of filters going in the respath
+		num_out_filters {int} -- Number of filters going out the respath
+		respath_length {int} -- length of ResPath
+		
+	'''
+
+	def __init__(self, num_in_filters, num_out_filters, respath_length):
+		super().__init__()
+		self.respath_length = respath_length
+		self.shortcuts = torch.nn.ModuleList([])
+		self.convs = torch.nn.ModuleList([])
+		self.bns = torch.nn.ModuleList([])
+		self.act = torch.nn.SELU(inplace=True)
+
+		for i in range(self.respath_length):
+			if(i==0):
+				self.shortcuts.append(Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (1,1), activation=False))
+				self.convs.append(Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (3,3),activation=True, inplace=True))
+			else:
+				self.shortcuts.append(Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False))
+				self.convs.append(Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True, inplace=True))
+		
+	def forward(self,x):
+		for i in range(self.respath_length):
+
+			shortcut = self.shortcuts[i](x)
+
+			x = self.convs[i](x)
+			x = self.act(x)
+
+			x = x + shortcut
+			x = self.act(x)
+
+		return x
 
 class MultiResUnet(torch.nn.Module):
 	'''
@@ -174,13 +254,12 @@ class MultiResUnet(torch.nn.Module):
 	Arguments:
 		input_channels {int} -- number of channels in image
 		num_classes {int} -- number of segmentation classes
-		alpha {float} -- alpha hyperparameter (default: 1.67)
+		alpha {float} -- alpha hyperparameter (default: 1.69)
 	
 	Returns:
 		[keras model] -- MultiResUNet model
 	'''
 	def __init__(self, input_channels, num_classes, alpha=1.69):
-		print ('hello from init MultiRes_v001')
 		super().__init__()
 		
 		self.alpha = alpha
@@ -256,41 +335,121 @@ class MultiResUnet(torch.nn.Module):
 
 		up6 = torch.cat([self.upsample6(x_multires5),x_multires4],axis=1)
 		x_multires6 = self.multiresblock6(up6)
-		'''
-		del x_multires5
-		del x_multires4
-		del up6
-		'''
 
 		up7 = torch.cat([self.upsample7(x_multires6),x_multires3],axis=1)
 		x_multires7 = self.multiresblock7(up7)
-		'''
-		del x_multires6
-		del x_multires3
-		del up7
-		'''
 
 		up8 = torch.cat([self.upsample8(x_multires7),x_multires2],axis=1)
 		x_multires8 = self.multiresblock8(up8)
-		'''
-		del x_multires7
-		del x_multires2
-		del up8
-		'''
 
 		up9 = torch.cat([self.upsample9(x_multires8),x_multires1],axis=1)
 		x_multires9 = self.multiresblock9(up9)
-		'''
+
+		out =  self.conv_final(x_multires9)
+		
+		return out
+
+class MultiResUnet_MemOpt(torch.nn.Module):
+	def __init__(self, input_channels, num_classes, alpha=1.69):
+		super().__init__()
+		
+		self.alpha = alpha
+		
+		# Encoder Path
+		self.multiresblock1 = Multiresblock_MemOpt(input_channels,32)
+		self.in_filters1 = int(32*self.alpha*0.167)+int(32*self.alpha*0.333)+int(32*self.alpha* 0.5)
+		self.pool1 =  torch.nn.MaxPool2d(2)
+		self.respath1 = Respath_MemOpt(self.in_filters1,32,respath_length=4)
+
+		self.multiresblock2 = Multiresblock_MemOpt(self.in_filters1,32*2)
+		self.in_filters2 = int(32*2*self.alpha*0.167)+int(32*2*self.alpha*0.333)+int(32*2*self.alpha* 0.5)
+		self.pool2 =  torch.nn.MaxPool2d(2)
+		self.respath2 = Respath_MemOpt(self.in_filters2,32*2,respath_length=3)
+	
+	
+		self.multiresblock3 =  Multiresblock_MemOpt(self.in_filters2,32*4)
+		self.in_filters3 = int(32*4*self.alpha*0.167)+int(32*4*self.alpha*0.333)+int(32*4*self.alpha* 0.5)
+		self.pool3 =  torch.nn.MaxPool2d(2)
+		self.respath3 = Respath_MemOpt(self.in_filters3,32*4,respath_length=2)
+	
+	
+		self.multiresblock4 = Multiresblock_MemOpt(self.in_filters3,32*8)
+		self.in_filters4 = int(32*8*self.alpha*0.167)+int(32*8*self.alpha*0.333)+int(32*8*self.alpha* 0.5)
+		self.pool4 =  torch.nn.MaxPool2d(2)
+		self.respath4 = Respath_MemOpt(self.in_filters4,32*8,respath_length=1)
+	
+	
+		self.multiresblock5 = Multiresblock_MemOpt(self.in_filters4,32*16)
+		self.in_filters5 = int(32*16*self.alpha*0.167)+int(32*16*self.alpha*0.333)+int(32*16*self.alpha* 0.5)
+	 
+		# Decoder path
+		self.upsample6 = torch.nn.ConvTranspose2d(self.in_filters5,32*8,kernel_size=(2,2),stride=(2,2))  
+		self.concat_filters1 = 32*8 *2
+		self.multiresblock6 = Multiresblock_MemOpt(self.concat_filters1,32*8)
+		self.in_filters6 = int(32*8*self.alpha*0.167)+int(32*8*self.alpha*0.333)+int(32*8*self.alpha* 0.5)
+
+		self.upsample7 = torch.nn.ConvTranspose2d(self.in_filters6,32*4,kernel_size=(2,2),stride=(2,2))  
+		self.concat_filters2 = 32*4 *2
+		self.multiresblock7 = Multiresblock_MemOpt(self.concat_filters2,32*4)
+		self.in_filters7 = int(32*4*self.alpha*0.167)+int(32*4*self.alpha*0.333)+int(32*4*self.alpha* 0.5)
+	
+		self.upsample8 = torch.nn.ConvTranspose2d(self.in_filters7,32*2,kernel_size=(2,2),stride=(2,2))
+		self.concat_filters3 = 32*2 *2
+		self.multiresblock8 = Multiresblock_MemOpt(self.concat_filters3,32*2)
+		self.in_filters8 = int(32*2*self.alpha*0.167)+int(32*2*self.alpha*0.333)+int(32*2*self.alpha* 0.5)
+	
+		self.upsample9 = torch.nn.ConvTranspose2d(self.in_filters8,32,kernel_size=(2,2),stride=(2,2))
+		self.concat_filters4 = 32 *2
+		self.multiresblock9 = Multiresblock_MemOpt(self.concat_filters4,32)
+		self.in_filters9 = int(32*self.alpha*0.167)+int(32*self.alpha*0.333)+int(32*self.alpha* 0.5)
+
+		self.conv_final = Conv2d_batchnorm(self.in_filters9, num_classes, kernel_size = (1,1), activation=False)
+
+	def forward(self, x : torch.Tensor)->torch.Tensor:
+		x_multires1 = self.multiresblock1(x)
+		x_pool1 = self.pool1(x_multires1)
+		x_multires1 = self.respath1(x_multires1)
+		
+		x_multires2 = self.multiresblock2(x_pool1)
+		x_pool2 = self.pool2(x_multires2)
+		x_multires2 = self.respath2(x_multires2)
+
+		x_multires3 = self.multiresblock3(x_pool2)
+		x_pool3 = self.pool3(x_multires3)
+		x_multires3 = self.respath3(x_multires3)
+
+		x_multires4 = self.multiresblock4(x_pool3)
+		x_pool4 = self.pool4(x_multires4)
+		x_multires4 = self.respath4(x_multires4)
+
+		x_multires5 = self.multiresblock5(x_pool4)
+
+		up6 = torch.cat([self.upsample6(x_multires5),x_multires4],axis=1)
+		x_multires6 = self.multiresblock6(up6)
+		del x_multires5
+		del x_multires4
+		del up6
+
+		up7 = torch.cat([self.upsample7(x_multires6),x_multires3],axis=1)
+		x_multires7 = self.multiresblock7(up7)
+		del x_multires6
+		del x_multires3
+		del up7
+
+		up8 = torch.cat([self.upsample8(x_multires7),x_multires2],axis=1)
+		x_multires8 = self.multiresblock8(up8)
+		del x_multires7
+		del x_multires2
+		del up8
+
+		up9 = torch.cat([self.upsample9(x_multires8),x_multires1],axis=1)
+		x_multires9 = self.multiresblock9(up9)
 		del x_multires8
 		del x_multires1
 		del up9
-		'''
 
 		out =  self.conv_final(x_multires9)
 
-		# print (f'\nmax: {torch.max(out):.4f}')
-		# print (f'min: {torch.min(out):.4f}')
-		
 		return out
 
 
@@ -301,4 +460,8 @@ class Model:
 
     @staticmethod
     def get_model():
+        return MultiResUnet_MemOpt
+
+    @staticmethod
+    def get_training_model():
         return MultiResUnet
