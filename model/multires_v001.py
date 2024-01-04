f8e0733f8c2d8c2c1816d8a693a3e98344487838
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-04 13:56:50 +0000

    update multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -231,16 +231,6 @@ class Respath(Module):
 		return x
 	
 class Respath4(Module):
-	'''
-	ResPath
-	
-	Arguments:
-		num_in_filters {int} -- Number of filters going in the respath
-		num_out_filters {int} -- Number of filters going out the respath
-		respath_length {int} -- length of ResPath
-		
-	'''
-
 	def __init__(self, num_in_filters, num_out_filters, respath_length):
 	
 		super().__init__()
@@ -278,16 +268,6 @@ class Respath4(Module):
 		return x
 
 class Respath3(Module):
-	'''
-	ResPath
-	
-	Arguments:
-		num_in_filters {int} -- Number of filters going in the respath
-		num_out_filters {int} -- Number of filters going out the respath
-		respath_length {int} -- length of ResPath
-		
-	'''
-
 	def __init__(self, num_in_filters, num_out_filters, respath_length):
 	
 		super().__init__()
@@ -318,16 +298,6 @@ class Respath3(Module):
 		return x
 
 class Respath2(Module):
-	'''
-	ResPath
-	
-	Arguments:
-		num_in_filters {int} -- Number of filters going in the respath
-		num_out_filters {int} -- Number of filters going out the respath
-		respath_length {int} -- length of ResPath
-		
-	'''
-
 	def __init__(self, num_in_filters, num_out_filters, respath_length):
 	
 		super().__init__()
@@ -351,16 +321,6 @@ class Respath2(Module):
 		return x
 
 class Respath1(Module):
-	'''
-	ResPath
-	
-	Arguments:
-		num_in_filters {int} -- Number of filters going in the respath
-		num_out_filters {int} -- Number of filters going out the respath
-		respath_length {int} -- length of ResPath
-		
-	'''
-
 	def __init__(self, num_in_filters, num_out_filters, respath_length):
 	
 		super().__init__()
@@ -376,88 +336,111 @@ class Respath1(Module):
 
 		return x
 
-class Respath_MemOpt(Module):
-	'''
-	ResPath
-	
-	Arguments:
-		num_in_filters {int} -- Number of filters going in the respath
-		num_out_filters {int} -- Number of filters going out the respath
-		respath_length {int} -- length of ResPath
-		
-	'''
 
+class Respath4_MemOpt(Module):
 	def __init__(self, num_in_filters, num_out_filters, respath_length):
+	
 		super().__init__()
-		print (f'respath memopt init {respath_length}')
-		self.respath_length = respath_length
-		self.shortcuts = torch.nn.ModuleList([])
-		self.convs = torch.nn.ModuleList([])
-		self.bns = torch.nn.ModuleList([])
-		self.act = torch.nn.SELU(inplace=True)
-
-		for i in range(self.respath_length):
-			if(i==0):
-				self.shortcuts.append(Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (1,1), activation=False))
-				self.convs.append(Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (3,3), activation=True, inplace=True))
-			else:
-				self.shortcuts.append(Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False))
-				self.convs.append(Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True, inplace=True))
+		self.act = torch.nn.SELU()
+		self.shortcut1 = Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.shortcut2 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.shortcut3 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.shortcut4 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.conv1 = Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (3,3),activation=True)
+		self.conv2 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True)
+		self.conv3 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True)
+		self.conv4 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True)
 		
-	def forward(self, x):
-		x_device = x.device
-		x_dtype = x.dtype
+	def forward(self,x):
+		shortcut = self.shortcut1(x)
+		x = self.conv1(x)
+		x = x + shortcut
+		x = self.act(x)
 
-		print ('respath forward')
-		print (f'x shape: {x.shape}')
-		print (f'respath length: {self.respath_length}')
+		shortcut = self.shortcut2(x)
+		x = self.conv2(x)
+		x = x + shortcut
+		x = self.act(x)
+		
+		shortcut = self.shortcut3(x)
+		x = self.conv3(x)
+		x = x + shortcut
+		x = self.act(x)
 
-		for i in range(self.respath_length):
-			print (f'respath iteration: {i}')
-			print (f'x shape: {x.shape}')
+		shortcut = self.shortcut4(x)
+		x = self.conv4(x)
+		x = x + shortcut
+		x = self.act(x)
 
-			try:
-				shortcut = self.shortcuts[i](x)
-				x = self.convs[i](x)
-				# x = self.act(x)
-				x = x + shortcut
-				del shortcut
-				x = self.act(x)
-			except:
-				print (f'out of mem in respath {i}')
-				try:
-					shortcut = self.shortcuts[i](x)
-					shortcut_cpu = shortcut.cpu()
-					del shortcut
-				except:
-					print (f'out of mem shortcut in respath {i} - cpu')
-					shortcut_conv_cpu = self.shortcuts[i].cpu()
-					shortcut_conv_cpu = shortcut_conv_cpu.to(torch.float32)
-					x_cpu = x
-					x_cpu = x_cpu.to(device='cpu', dtype=torch.float32)
-					shortcut_cpu = shortcut_conv_cpu(x_cpu)
-					del x_cpu
-				try:
-					x = self.convs[i](x)
-					x_cpu = x.cpu()
-					del x
-				except:
-					print (f'out of mem conv in respath {i} - cpu')
-					x_cpu = x
-					del x
-					x_cpu = x_cpu.to(device='cpu', dtype=torch.float32)
-					conv_cpu = self.convs[i].cpu()
-					conv_cpu = conv_cpu.to(torch.float32)
-					x_cpu = conv_cpu(x_cpu)
-				x_cpu = x_cpu + shortcut_cpu
-				x_cpu = self.act(x_cpu)
-				x = x_cpu.to(x_device, dtype=x_dtype)
+		return x
 
-			try:
-				torch.cuda.empty_cache()
-			except:
-				pass
-			
+class Respath3_MemOpt(Module):
+	def __init__(self, num_in_filters, num_out_filters, respath_length):
+	
+		super().__init__()
+		self.act = torch.nn.SELU()
+		self.shortcut1 = Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.shortcut2 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.shortcut3 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.conv1 = Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (3,3),activation=True)
+		self.conv2 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True)
+		self.conv3 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True)
+		
+	def forward(self,x):
+		shortcut = self.shortcut1(x)
+		x = self.conv1(x)
+		x = x + shortcut
+		x = self.act(x)
+
+		shortcut = self.shortcut2(x)
+		x = self.conv2(x)
+		x = x + shortcut
+		x = self.act(x)
+
+		shortcut = self.shortcut3(x)
+		x = self.conv3(x)
+		x = x + shortcut
+		x = self.act(x)
+
+		return x
+
+class Respath2_MemOpt(Module):
+	def __init__(self, num_in_filters, num_out_filters, respath_length):
+	
+		super().__init__()
+		self.act = torch.nn.SELU()
+		self.shortcut1 = Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.shortcut2 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.conv1 = Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (3,3),activation=True)
+		self.conv2 = Conv2d_batchnorm(num_out_filters, num_out_filters, kernel_size = (3,3), activation=True)
+		
+	def forward(self,x):
+		shortcut = self.shortcut1(x)
+		x = self.conv1(x)
+		x = x + shortcut
+		x = self.act(x)
+
+		shortcut = self.shortcut2(x)
+		x = self.conv2(x)
+		x = x + shortcut
+		x = self.act(x)
+
+		return x
+
+class Respath1_MemOpt(Module):
+	def __init__(self, num_in_filters, num_out_filters, respath_length):
+	
+		super().__init__()
+		self.act = torch.nn.SELU()
+		self.shortcut1 = Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (1,1), activation=False)
+		self.conv1 = Conv2d_batchnorm(num_in_filters, num_out_filters, kernel_size = (3,3),activation=True)
+		
+	def forward(self,x):
+		shortcut = self.shortcut1(x)
+		x = self.conv1(x)
+		x = x + shortcut
+		x = self.act(x)
+
 		return x
 
 class MultiResUnet(Module):
@@ -571,24 +554,24 @@ class MultiResUnet_MemOpt(Module):
 		self.multiresblock1 = Multiresblock_MemOpt(input_channels,32)
 		self.in_filters1 = int(32*self.alpha*0.167)+int(32*self.alpha*0.333)+int(32*self.alpha* 0.5)
 		self.pool1 =  torch.nn.MaxPool2d(2)
-		self.respath1 = Respath_MemOpt(self.in_filters1,32,respath_length=4)
+		self.respath1 = Respath4_MemOpt(self.in_filters1,32,respath_length=4)
 
 		self.multiresblock2 = Multiresblock_MemOpt(self.in_filters1,32*2)
 		self.in_filters2 = int(32*2*self.alpha*0.167)+int(32*2*self.alpha*0.333)+int(32*2*self.alpha* 0.5)
 		self.pool2 =  torch.nn.MaxPool2d(2)
-		self.respath2 = Respath_MemOpt(self.in_filters2,32*2,respath_length=3)
+		self.respath2 = Respath3_MemOpt(self.in_filters2,32*2,respath_length=3)
 	
 	
 		self.multiresblock3 =  Multiresblock_MemOpt(self.in_filters2,32*4)
 		self.in_filters3 = int(32*4*self.alpha*0.167)+int(32*4*self.alpha*0.333)+int(32*4*self.alpha* 0.5)
 		self.pool3 =  torch.nn.MaxPool2d(2)
-		self.respath3 = Respath_MemOpt(self.in_filters3,32*4,respath_length=2)
+		self.respath3 = Respath2_MemOpt(self.in_filters3,32*4,respath_length=2)
 	
 	
 		self.multiresblock4 = Multiresblock_MemOpt(self.in_filters3,32*8)
 		self.in_filters4 = int(32*8*self.alpha*0.167)+int(32*8*self.alpha*0.333)+int(32*8*self.alpha* 0.5)
 		self.pool4 =  torch.nn.MaxPool2d(2)
-		self.respath4 = Respath_MemOpt(self.in_filters4,32*8,respath_length=1)
+		self.respath4 = Respath1_MemOpt(self.in_filters4,32*8,respath_length=1)
 	
 		self.multiresblock5 = Multiresblock_MemOpt(self.in_filters4,32*16)
 		self.in_filters5 = int(32*16*self.alpha*0.167)+int(32*16*self.alpha*0.333)+int(32*16*self.alpha* 0.5)
@@ -618,62 +601,64 @@ class MultiResUnet_MemOpt(Module):
 		self.msg = Message(msg_queue)
 
 	def forward(self, x):
-		print ('enc start')
-		x_multires1 = self.multiresblock1(x)
-		print ('enc multiresblock1')
-		x_pool1 = self.pool1(x_multires1)
-		print ('enc pool1')
-		x_multires1 = self.respath1(x_multires1)
+		try:
+			print ('enc start')
+			x_multires1 = self.multiresblock1(x)
+			print ('enc multiresblock1')
+			x_pool1 = self.pool1(x_multires1)
+			print ('enc pool1')
+			x_multires1 = self.respath1(x_multires1)
+			print ('enc step 01 completed')
+			
+			x_multires2 = self.multiresblock2(x_pool1)
+			del x_pool1
+			x_pool2 = self.pool2(x_multires2)
+			x_multires2 = self.respath2(x_multires2)
 
-		print ('enc step 01')
-		
-		x_multires2 = self.multiresblock2(x_pool1)
-		del x_pool1
-		x_pool2 = self.pool2(x_multires2)
-		x_multires2 = self.respath2(x_multires2)
+			print ('enc step 02 completed')
 
-		print ('enc step 02')
+			x_multires3 = self.multiresblock3(x_pool2)
+			del x_pool2
+			x_pool3 = self.pool3(x_multires3)
+			x_multires3 = self.respath3(x_multires3)
 
-		x_multires3 = self.multiresblock3(x_pool2)
-		del x_pool2
-		x_pool3 = self.pool3(x_multires3)
-		x_multires3 = self.respath3(x_multires3)
+			print ('enc step 03 completed')
 
-		print ('enc step 03')
+			x_multires4 = self.multiresblock4(x_pool3)
+			del x_pool3
+			x_pool4 = self.pool4(x_multires4)
+			x_multires4 = self.respath4(x_multires4)
 
-		x_multires4 = self.multiresblock4(x_pool3)
-		del x_pool3
-		x_pool4 = self.pool4(x_multires4)
-		x_multires4 = self.respath4(x_multires4)
+			print ('enc step 04 completed')
 
-		print ('enc step 04')
+			x_multires5 = self.multiresblock5(x_pool4)
+			del x_pool4
+			
+			up6 = torch.cat([self.upsample6(x_multires5),x_multires4],axis=1)
+			x_multires6 = self.multiresblock6(up6)
+			del x_multires5
+			del x_multires4
+			del up6
 
-		x_multires5 = self.multiresblock5(x_pool4)
-		del x_pool4
-		
-		up6 = torch.cat([self.upsample6(x_multires5),x_multires4],axis=1)
-		x_multires6 = self.multiresblock6(up6)
-		del x_multires5
-		del x_multires4
-		del up6
+			up7 = torch.cat([self.upsample7(x_multires6),x_multires3],axis=1)
+			x_multires7 = self.multiresblock7(up7)
+			del x_multires6
+			del x_multires3
+			del up7
 
-		up7 = torch.cat([self.upsample7(x_multires6),x_multires3],axis=1)
-		x_multires7 = self.multiresblock7(up7)
-		del x_multires6
-		del x_multires3
-		del up7
+			up8 = torch.cat([self.upsample8(x_multires7),x_multires2],axis=1)
+			x_multires8 = self.multiresblock8(up8)
+			del x_multires7
+			del x_multires2
+			del up8
 
-		up8 = torch.cat([self.upsample8(x_multires7),x_multires2],axis=1)
-		x_multires8 = self.multiresblock8(up8)
-		del x_multires7
-		del x_multires2
-		del up8
-
-		up9 = torch.cat([self.upsample9(x_multires8),x_multires1],axis=1)
-		x_multires9 = self.multiresblock9(up9)
-		del x_multires8
-		del x_multires1
-		del up9
+			up9 = torch.cat([self.upsample9(x_multires8),x_multires1],axis=1)
+			x_multires9 = self.multiresblock9(up9)
+			del x_multires8
+			del x_multires1
+			del up9
+		except:
+			print (f'GPU Mem low failure')
 
 		out =  self.conv_final(x_multires9)
 
