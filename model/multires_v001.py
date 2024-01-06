9fcbd4bba371835b77ace76340508c9b81b0c5a4
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-06 18:43:06 +0000

    update multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -108,9 +108,11 @@ class Conv2d_MemOPT(Module):
 		n, d, h, w = x.shape
 		out = torch.empty(n, self.num_out_filters, h, w, device=x_device, dtype=x_dtype)
 		slice_width = w // self.num_slices
-		for w_index in range(0, self.num_slices):
-			out[:, :, :, w_index:w_index+slice_width] = self.conv1(x[:, :, :, w_index:w_index+slice_width])
 		x = self.conv1(x)
+		for w_index in range(0, self.num_slices):
+			out[:, :, :, w_index:w_index+slice_width] = x[:, :, :, w_index:w_index+slice_width]
+			# out[:, :, :, w_index:w_index+slice_width] = self.conv1(x[:, :, :, w_index:w_index+slice_width])
+		
 		print (torch.equal(out, x))
 		return out
 
