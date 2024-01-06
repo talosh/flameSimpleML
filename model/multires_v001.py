2023acb87c519504db13ff5effe934a54fd4bcd1
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-06 19:35:52 +0000

    update multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -157,10 +157,11 @@ class Conv2d_ReLU_MemOPT(Module):
 		print (f'x.shape: {x.shape}')
 		print (f'first slice conv shape: {slice_conv[:, :, :, :slice_width].shape}')
 		# out[:, :, :, 0:slice_width] = self.conv1(x[:, :, :, 0:slice_width + 1])[:, :, :, :slice_width]
-		for w_index in range(1, self.num_slices - 2):
-			out[:, :, :, w_index*slice_width:w_index*slice_width+slice_width] = self.conv1(x[:, :, :, w_index*slice_width - 1 : w_index*slice_width+slice_width + 1])[:, :, :, 1:slice_width]
+		# for w_index in range(1, self.num_slices - 2):
+		#	out[:, :, :, w_index*slice_width:w_index*slice_width+slice_width] = self.conv1(x[:, :, :, w_index*slice_width - 1 : w_index*slice_width+slice_width + 1])[:, :, :, 1:slice_width]
 		# out[:, :, :, self.num_slices*slice_width:self.num_slices*slice_width+slice_width] = self.conv1(x[:, :, :, self.num_slices*slice_width - 1 : self.num_slices*slice_width+slice_width])[:, :, :, 1:slice_width]
 		whole = self.conv1(x)
+		print (f'whole shape: {whole.shape}')
 		print (f'x.shape: {x.shape}, {torch.equal(out, whole)}')
 
 		out = self.act(out)
