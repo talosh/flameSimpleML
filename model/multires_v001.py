3627b428bff82a6912938d80522742804b6e2c19
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-06 19:00:25 +0000

    update multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -112,9 +112,9 @@ class Conv2d_MemOPT(Module):
 		# for w_index in range(0, self.num_slices):
 		# 	out[:, :, :, w_index:w_index+slice_width] = self.conv1(x[:, :, :, w_index:w_index+slice_width])
 		# out[:, :, :, :self.num_slices * slice_width] = x[:, :, :, :self.num_slices * slice_width]
-		out[:, :, :, :w/2] = self.conv1(x[:, :, :, :w/2])
+		out[:, :, :, :w//2] = self.conv1(x[:, :, :, :w//2])
 		whole = self.conv1(x)
-		out[:, :, :, w/2:w] = whole[:, :, :, w/2:w]
+		out[:, :, :, w//2:w] = whole[:, :, :, w//2:w]
 		print (f'x.shape: {x.shape}, out.shape{out.shape}: {torch.equal(out, whole)}')
 		return out
 
