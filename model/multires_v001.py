e51d79b245db1781f9b66c54d02fcb4e0473060e
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-07 20:49:54 +0000

    update multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -103,17 +103,19 @@ class Conv2d_MemOPT(Module):
 			)
 	
 	def forward(self,x):
+		input_device = x.device
+		input_dtype = x.dtype
 		model_device = self.conv1.weight.device
 		model_dtype = self.conv1.weight.dtype
 		n, d, h, w = x.shape
 		slice_width = w // self.num_slices
 
-		out = torch.empty(n, self.num_out_filters, h, w, device='cpu', dtype=model_dtype)
+		out = torch.empty(n, self.num_out_filters, h, w, device=input_device, dtype=input_dtype)
 
 		for w_index in range(0, self.num_slices):
 			input_slice = x[:, :, :, w_index*slice_width:w_index*slice_width+slice_width].clone().detach().to(device=model_device, dtype=model_dtype)
 			output_slice = self.conv1(input_slice)
-			out[:, :, :, w_index*slice_width:w_index*slice_width+slice_width] = output_slice.clone().detach().cpu()
+			out[:, :, :, w_index*slice_width:w_index*slice_width+slice_width] = output_slice.clone().detach().to(device=input_device, dtype=input_dtype)
 
 		del x, input_slice, output_slice
 		return out
@@ -154,28 +156,30 @@ class Conv2d_ReLU_MemOPT(Module):
 		self.act = torch.nn.SELU(inplace = True)
 	
 	def forward(self,x):
+		input_device = x.device
+		input_dtype = x.dtype
 		model_device = self.conv1.weight.device
 		model_dtype = self.conv1.weight.dtype
 		n, d, h, w = x.shape
 		slice_width = w // self.num_slices
 
-		out = torch.empty(n, self.num_out_filters, h, w, device='cpu', dtype=model_dtype)
+		out = torch.empty(n, self.num_out_filters, h, w, device=input_device, dtype=input_dtype)
 
 		input_slice = x[:, :, :, :slice_width + 2].clone().detach().to(device=model_device, dtype=model_dtype)
 		output_slice = self.conv1(input_slice)[:, :, :, :slice_width]
 		output_slice = self.act(output_slice)
-		out[:, :, :, :slice_width] = output_slice.clone().detach().cpu()
+		out[:, :, :, :slice_width] = output_slice.clone().detach().to(device=input_device, dtype=input_dtype)
 
 		for w_index in range(1, self.num_slices - 1):
 			input_slice = x[:, :, :, w_index*slice_width - 2 : w_index*slice_width+slice_width + 2].clone().detach().to(device=model_device, dtype=model_dtype)
 			output_slice = self.conv1(input_slice)[:, :, :, 2:slice_width+2]
 			output_slice = self.act(output_slice)
-			out[:, :, :, w_index*slice_width:w_index*slice_width+slice_width] = output_slice.clone().detach().cpu()
+			out[:, :, :, w_index*slice_width:w_index*slice_width+slice_width] = output_slice.clone().detach().to(device=input_device, dtype=input_dtype)
 
 		input_slice = x[:, :, :, w-slice_width-2:].clone().detach().to(device=model_device, dtype=model_dtype)
 		output_slice = self.conv1(input_slice)[:, :, :, 2:slice_width+2]
 		output_slice = self.act(output_slice)
-		out[:, :, :, w-slice_width:] = output_slice.clone().detach().cpu()
+		out[:, :, :, w-slice_width:] = output_slice.clone().detach().to(device=input_device, dtype=input_dtype)
 
 		del x, input_slice, output_slice
 		return out
