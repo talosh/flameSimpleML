616402d6db3092f53244503343dfd3c87f058580
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-07 15:14:08 +0000

    update multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -842,7 +842,7 @@ class MultiResUnet_MemOpt(Module):
 		x_multires1 = self.multiresblock1(x)
 		print (f'x_multires1 device: {x_multires1.device}')
 		x_pool1 = self.pool1(x_multires1)
- 		print (f'x_pool1.device {x_pool1.device}')
+		print (f'x_pool1.device {x_pool1.device}')
 		x_multires1 = self.respath1(x_multires1)
 		
 
