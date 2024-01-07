283fc53db48e758f4332087eafb2905bdd13619f
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-07 16:45:31 +0000

    update multires_v001.py

--- model/multires_v001.py
+++ model/multires_v001.py
@@ -160,7 +160,7 @@ class Conv2d_ReLU_MemOPT(Module):
 		slice_width = w // self.num_slices
 		input_slice = x[:, :, :, :slice_width + 2].to(device=model_device, dtype=model_dtype)
 		# output_slice = out[:, :, :, :slice_width + 2].to(device=model_device, dtype=model_dtype)[:, :, :, :slice_width]
-		output_slice = self.conv2(input_slice)[:, :, :, :slice_width]
+		output_slice = self.conv1(input_slice)[:, :, :, :slice_width]
 		del input_slice
 		output_slice = self.act(output_slice)
 		out[:, :, :, :slice_width] = output_slice.cpu()
@@ -169,7 +169,7 @@ class Conv2d_ReLU_MemOPT(Module):
 		for w_index in range(1, self.num_slices - 1):
 			input_slice = x[:, :, :, w_index*slice_width - 2 : w_index*slice_width+slice_width + 2].to(device=model_device, dtype=model_dtype)
 			output_slice = out[:, :, :, w_index*slice_width - 2 : w_index*slice_width+slice_width + 2].to(device=model_device, dtype=model_dtype)[:, :, :, 2:slice_width+2]
-			# output_slice = self.conv2(input_slice)[:, :, :, 2:slice_width+2]
+			# output_slice = self.conv1(input_slice)[:, :, :, 2:slice_width+2]
 			del input_slice
 			output_slice = self.act(output_slice)
 			out[:, :, :, w_index*slice_width:w_index*slice_width+slice_width] = output_slice.cpu()
@@ -177,7 +177,7 @@ class Conv2d_ReLU_MemOPT(Module):
 			# out[:, :, :, w_index*slice_width:w_index*slice_width+slice_width] = self.conv1(x[:, :, :, w_index*slice_width - 2 : w_index*slice_width+slice_width + 2])[:, :, :, 2:slice_width+2]
 		input_slice = x[:, :, :, w-slice_width-2:].to(device=model_device, dtype=model_dtype)
 		output_slice = out[:, :, :, w-slice_width-2:].to(device=model_device, dtype=model_dtype)[:, :, :, 2:slice_width+2]
-		# output_slice = self.conv2(input_slice)[:, :, :, 2:slice_width+2]
+		# output_slice = self.conv1(input_slice)[:, :, :, 2:slice_width+2]
 		del input_slice
 		output_slice = self.act(output_slice)
 		out[:, :, :, w-slice_width:] = output_slice.cpu()
