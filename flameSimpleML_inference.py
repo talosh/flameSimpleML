c997e491b1382998602eeeb55a60f7a0a3662145
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-08 00:45:58 +0000

    update flameSimpleML_inference.py

--- flameSimpleML_inference.py
+++ flameSimpleML_inference.py
@@ -2136,46 +2136,46 @@ class flameSimpleMLInference(QtWidgets.QWidget):
         # print(torch.cuda.memory_summary(device=None, abbreviated=False))
         '''
 
-        def normalize_values(self, image_array):
-            import torch
+    def normalize_values(self, image_array):
+        import torch
 
-            def custom_bend(x):
-                linear_part = x
-                exp_positive = torch.pow( x, 1 / 4 )
-                exp_negative = -torch.pow( -x, 1 / 4 )
-                return torch.where(x > 1, exp_positive, torch.where(x < -1, exp_negative, linear_part))
+        def custom_bend(x):
+            linear_part = x
+            exp_positive = torch.pow( x, 1 / 4 )
+            exp_negative = -torch.pow( -x, 1 / 4 )
+            return torch.where(x > 1, exp_positive, torch.where(x < -1, exp_negative, linear_part))
 
-            # transfer (0.0 - 1.0) onto (-1.0 - 1.0) for tanh
-            image_array = (image_array * 2) - 1
-            # bend values below -1.0 and above 1.0 exponentially so they are not larger then (-4.0 - 4.0)
-            image_array = custom_bend(image_array)
-            # bend everything to fit -1.0 - 1.0 with hyperbolic tanhent
-            image_array = torch.tanh(image_array)
-            # move it to 0.0 - 1.0 range
-            image_array = (image_array + 1) / 2
+        # transfer (0.0 - 1.0) onto (-1.0 - 1.0) for tanh
+        image_array = (image_array * 2) - 1
+        # bend values below -1.0 and above 1.0 exponentially so they are not larger then (-4.0 - 4.0)
+        image_array = custom_bend(image_array)
+        # bend everything to fit -1.0 - 1.0 with hyperbolic tanhent
+        image_array = torch.tanh(image_array)
+        # move it to 0.0 - 1.0 range
+        image_array = (image_array + 1) / 2
 
-            return image_array
-        
-        def restore_normalized_values(self, image_array):
-            import torch
+        return image_array
+    
+    def restore_normalized_values(self, image_array):
+        import torch
 
-            def custom_de_bend(x):
-                linear_part = x
-                inv_positive = torch.pow( x, 4 )
-                inv_negative = -torch.pow( -x, 4 )
-                return torch.where(x > 1, inv_positive, torch.where(x < -1, inv_negative, linear_part))
+        def custom_de_bend(x):
+            linear_part = x
+            inv_positive = torch.pow( x, 4 )
+            inv_negative = -torch.pow( -x, 4 )
+            return torch.where(x > 1, inv_positive, torch.where(x < -1, inv_negative, linear_part))
 
-            epsilon = torch.tensor(4e-8, dtype=torch.float32).to(image_array.device)
-            # clamp image befor arctanh
-            image_array = torch.clamp((image_array * 2) - 1, -1.0 + epsilon, 1.0 - epsilon)
-            # restore values from tanh  s-curve
-            image_array = torch.arctanh(image_array)
-            # restore custom bended values
-            image_array = custom_de_bend(image_array)
-            # move it to 0.0 - 1.0 range
-            image_array = ( image_array + 1.0) / 2.0
+        epsilon = torch.tensor(4e-8, dtype=torch.float32).to(image_array.device)
+        # clamp image befor arctanh
+        image_array = torch.clamp((image_array * 2) - 1, -1.0 + epsilon, 1.0 - epsilon)
+        # restore values from tanh  s-curve
+        image_array = torch.arctanh(image_array)
+        # restore custom bended values
+        image_array = custom_de_bend(image_array)
+        # move it to 0.0 - 1.0 range
+        image_array = ( image_array + 1.0) / 2.0
 
-            return image_array
+        return image_array
 
     def apply_model(self, src_image_data):
         import torch
