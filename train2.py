e061eeda2c5fe1fa4ca7a7935c37680cb09885f8
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-09 10:34:49 +0000

    update train2.py

--- train2.py
+++ train2.py
@@ -308,7 +308,7 @@ while epoch < num_epochs + 1:
         rgb_output_restored_clmp = restore(torch.clamp(rgb_output, min=0.04, max=0.96))
         rgb_after_restored_clmp = restore(torch.clamp(rgb_after, min=0.04, max=0.96))
 
-        loss = 0.8 * criterion_mse(rgb_output, rgb_after) + 0.2 * criterion_mse(bend_highlights(rgb_output_restored_clmp, 4), bend_highlights(rgb_after_restored_clmp, 4)) # + 0.5 * criterion_mse(rgb_output_restored_clmp, rgb_after_restored_clmp)
+        loss = 0.8 * criterion_mse(rgb_output, rgb_after) + 0.2 * criterion_l1(rgb_output_restored_clmp, rgb_after_restored_clmp) # + 0.5 * criterion_mse(rgb_output_restored_clmp, rgb_after_restored_clmp)
         loss_l1 = criterion_l1(rgb_output_restored, rgb_after_restored)
         loss_l1_str = str(f'{loss_l1.item():.4f}')
 
