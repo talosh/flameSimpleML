8cb7ec4e6fda495119b02af647bb867e3bae0834
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-10 18:57:23 +0000

    update train2.py

--- train2.py
+++ train2.py
@@ -153,7 +153,7 @@ read_thread.start()
 log_path = 'train_log'
 num_epochs = 4444
 warmup_epochs = 9
-lr = 9e-4
+lr = 4e-3
 lr_dive = 10
 batch_size = 1
 data_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, num_workers=8, pin_memory=True)
