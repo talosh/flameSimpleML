1e8d6b6cdf4c7e218c7c2494b141c19911d96cbc
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-11 11:17:29 +0000

    update train2.py

--- train2.py
+++ train2.py
@@ -331,7 +331,7 @@ while epoch < num_epochs + 1:
         data_time_str = str(f'{data_time:.2f}')
         train_time_str = str(f'{train_time:.2f}')
         current_lr_str = str(f'{optimizer.param_groups[0]["lr"]:.4e}')
-        print (scheduler.get_last_lr())
+        print (f'{scheduler.get_last_lr()}')
         # current_lr_str = str(f'{scheduler.get_last_lr():.4e}')
 
         epoch_time = time.time() - start_timestamp
