0f8c016fc0239b2b616abab123f563fb508eca51
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-13 23:15:59 +0000

    update dataset.py and train.py

--- train.py
+++ train.py
@@ -366,6 +366,7 @@ class myDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         img0, img1 = self.getimg(index)
 
+        '''
         q = random.uniform(0, 1)
         if q < 0.5:
             img0, img1 = self.crop(img0, img1, self.h, self.w)
@@ -394,6 +395,11 @@ class myDataset(torch.utils.data.Dataset):
         elif p < 0.75:
             img0 = torch.flip(img0.transpose(0, 1), [0])
             img1 = torch.flip(img1.transpose(0, 1), [0])
+        '''
+
+        img0, img1 = self.crop(img0, img1, self.h, self.w)
+        img0 = torch.from_numpy(img0.copy()).permute(2, 0, 1)
+        img1 = torch.from_numpy(img1.copy()).permute(2, 0, 1)
 
         return img0, img1
 
