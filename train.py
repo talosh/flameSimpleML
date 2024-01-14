03182fa09e7f8322822f8fe0a378e3032c74528b
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-14 01:31:42 +0000

    update flameSimpleML_inference.py, train.py and train2.py

--- train.py
+++ train.py
@@ -721,6 +721,7 @@ def main():
         days = int(epoch_time // (24 * 3600))
         hours = int((epoch_time % (24 * 3600)) // 3600)
         minutes = int((epoch_time % 3600) // 60)
+        print (f'\r {" "*120}', end='')
         print(f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Min: {min(epoch_loss):.4f} Avg: {smoothed_loss:.4f}, Max: {max(epoch_loss):.4f}')
         steps_loss = []
         epoch_loss = []
