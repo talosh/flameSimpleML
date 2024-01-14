dea01ef7847323f5a3a83d443239e72e3d423f2c
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-14 00:46:04 +0000

    delete 11 files and update 1 file

--- train.py
+++ train.py
@@ -263,92 +263,6 @@ class MinExrReader:
         assert buf.nleft() > 0 and buf.peek() != 0x00, 'Failed to read offset.'
         return struct.unpack('<Q', buf.read(8))[0]
 
-def write_exr(image_data, filename, half_float = False, pixelAspectRatio = 1.0):
-    height, width, depth = image_data.shape
-    red = image_data[:, :, 0]
-    green = image_data[:, :, 1]
-    blue = image_data[:, :, 2]
-    if depth > 3:
-        alpha = image_data[:, :, 3]
-    else:
-        alpha = np.array([])
-
-    channels_list = ['B', 'G', 'R'] if not alpha.size else ['A', 'B', 'G', 'R']
-
-    MAGIC = 20000630
-    VERSION = 2
-    UINT = 0
-    HALF = 1
-    FLOAT = 2
-
-    def write_attr(f, name, type, value):
-        f.write(name.encode('utf-8') + b'\x00')
-        f.write(type.encode('utf-8') + b'\x00')
-        f.write(struct.pack('<I', len(value)))
-        f.write(value)
-
-    def get_channels_attr(channels_list):
-        channel_list = b''
-        for channel_name in channels_list:
-            name_padded = channel_name[:254] + '\x00'
-            bit_depth = 1 if half_float else 2
-            pLinear = 0
-            reserved = (0, 0, 0)  # replace with your values if needed
-            xSampling = 1  # replace with your value
-            ySampling = 1  # replace with your value
-            channel_list += struct.pack(
-                f"<{len(name_padded)}s i B 3B 2i",
-                name_padded.encode(), 
-                bit_depth, 
-                pLinear, 
-                *reserved, 
-                xSampling, 
-                ySampling
-                )
-        channel_list += struct.pack('c', b'\x00')
-
-            # channel_list += (f'{i}\x00').encode('utf-8')
-            # channel_list += struct.pack("<i4B", HALF, 1, 1, 0, 0)
-        return channel_list
-    
-    def get_box2i_attr(x_min, y_min, x_max, y_max):
-        return struct.pack('<iiii', x_min, y_min, x_max, y_max)
-
-    with open(filename, 'wb') as f:
-        # Magic number and version field
-        f.write(struct.pack('I', 20000630))  # Magic number
-        f.write(struct.pack('H', 2))  # Version field
-        f.write(struct.pack('H', 0))  # Version field
-        write_attr(f, 'channels', 'chlist', get_channels_attr(channels_list))
-        write_attr(f, 'compression', 'compression', b'\x00')  # no compression
-        write_attr(f, 'dataWindow', 'box2i', get_box2i_attr(0, 0, width - 1, height - 1))
-        write_attr(f, 'displayWindow', 'box2i', get_box2i_attr(0, 0, width - 1, height - 1))
-        write_attr(f, 'lineOrder', 'lineOrder', b'\x00')  # increasing Y
-        write_attr(f, 'pixelAspectRatio', 'float', struct.pack('<f', pixelAspectRatio))
-        write_attr(f, 'screenWindowCenter', 'v2f', struct.pack('<ff', 0.0, 0.0))
-        write_attr(f, 'screenWindowWidth', 'float', struct.pack('<f', 1.0))
-        f.write(b'\x00')  # end of header
-
-        # Scan line offset table size and position
-        line_offset_pos = f.tell()
-        pixel_data_start = line_offset_pos + 8 * height
-        bytes_per_channel = 2 if half_float else 4
-        # each scan line starts with 4 bytes for y coord and 4 bytes for pixel data size
-        bytes_per_scan_line = width * len(channels_list) * bytes_per_channel + 8 
-
-        for y in range(height):
-            f.write(struct.pack('<Q', pixel_data_start + y * bytes_per_scan_line))
-
-        channel_data = {'R': red, 'G': green, 'B': blue, 'A': alpha}
-
-        # Pixel data
-        for y in range(height):
-            f.write(struct.pack('I', y))  # Line number
-            f.write(struct.pack('I', bytes_per_channel * len(channels_list) * width))  # Pixel data size
-            for channel in sorted(channels_list):
-                f.write(channel_data[channel][y].tobytes())
-        f.close
-
 class myDataset(torch.utils.data.Dataset):
     def __init__(self, data_root):
         self.data_root = data_root
@@ -482,6 +396,127 @@ class myDataset(torch.utils.data.Dataset):
 
         return img0, img1
 
+def write_exr(image_data, filename, half_float = False, pixelAspectRatio = 1.0):
+    height, width, depth = image_data.shape
+    red = image_data[:, :, 0]
+    green = image_data[:, :, 1]
+    blue = image_data[:, :, 2]
+    if depth > 3:
+        alpha = image_data[:, :, 3]
+    else:
+        alpha = np.array([])
+
+    channels_list = ['B', 'G', 'R'] if not alpha.size else ['A', 'B', 'G', 'R']
+
+    MAGIC = 20000630
+    VERSION = 2
+    UINT = 0
+    HALF = 1
+    FLOAT = 2
+
+    def write_attr(f, name, type, value):
+        f.write(name.encode('utf-8') + b'\x00')
+        f.write(type.encode('utf-8') + b'\x00')
+        f.write(struct.pack('<I', len(value)))
+        f.write(value)
+
+    def get_channels_attr(channels_list):
+        channel_list = b''
+        for channel_name in channels_list:
+            name_padded = channel_name[:254] + '\x00'
+            bit_depth = 1 if half_float else 2
+            pLinear = 0
+            reserved = (0, 0, 0)  # replace with your values if needed
+            xSampling = 1  # replace with your value
+            ySampling = 1  # replace with your value
+            channel_list += struct.pack(
+                f"<{len(name_padded)}s i B 3B 2i",
+                name_padded.encode(), 
+                bit_depth, 
+                pLinear, 
+                *reserved, 
+                xSampling, 
+                ySampling
+                )
+        channel_list += struct.pack('c', b'\x00')
+
+            # channel_list += (f'{i}\x00').encode('utf-8')
+            # channel_list += struct.pack("<i4B", HALF, 1, 1, 0, 0)
+        return channel_list
+    
+    def get_box2i_attr(x_min, y_min, x_max, y_max):
+        return struct.pack('<iiii', x_min, y_min, x_max, y_max)
+
+    with open(filename, 'wb') as f:
+        # Magic number and version field
+        f.write(struct.pack('I', 20000630))  # Magic number
+        f.write(struct.pack('H', 2))  # Version field
+        f.write(struct.pack('H', 0))  # Version field
+        write_attr(f, 'channels', 'chlist', get_channels_attr(channels_list))
+        write_attr(f, 'compression', 'compression', b'\x00')  # no compression
+        write_attr(f, 'dataWindow', 'box2i', get_box2i_attr(0, 0, width - 1, height - 1))
+        write_attr(f, 'displayWindow', 'box2i', get_box2i_attr(0, 0, width - 1, height - 1))
+        write_attr(f, 'lineOrder', 'lineOrder', b'\x00')  # increasing Y
+        write_attr(f, 'pixelAspectRatio', 'float', struct.pack('<f', pixelAspectRatio))
+        write_attr(f, 'screenWindowCenter', 'v2f', struct.pack('<ff', 0.0, 0.0))
+        write_attr(f, 'screenWindowWidth', 'float', struct.pack('<f', 1.0))
+        f.write(b'\x00')  # end of header
+
+        # Scan line offset table size and position
+        line_offset_pos = f.tell()
+        pixel_data_start = line_offset_pos + 8 * height
+        bytes_per_channel = 2 if half_float else 4
+        # each scan line starts with 4 bytes for y coord and 4 bytes for pixel data size
+        bytes_per_scan_line = width * len(channels_list) * bytes_per_channel + 8 
+
+        for y in range(height):
+            f.write(struct.pack('<Q', pixel_data_start + y * bytes_per_scan_line))
+
+        channel_data = {'R': red, 'G': green, 'B': blue, 'A': alpha}
+
+        # Pixel data
+        for y in range(height):
+            f.write(struct.pack('I', y))  # Line number
+            f.write(struct.pack('I', bytes_per_channel * len(channels_list) * width))  # Pixel data size
+            for channel in sorted(channels_list):
+                f.write(channel_data[channel][y].tobytes())
+        f.close
+
+def normalize(img) :
+    def custom_bend(x) :
+        linear_part = x
+        exp_positive = torch.pow( x, 1 / 4 )
+        exp_negative = - torch.pow( -x, 1 / 4 )
+        return torch.where(x > 1, exp_positive, torch.where(x < -1, exp_negative, linear_part))
+    
+    img = (img * 2) - 1
+    img = custom_bend(img)
+    img = torch.tanh(img)
+    img = (img + 1) / 2
+    return img
+
+def restore_normalized_values(image_array, torch = None):
+    if torch is None:
+        import torch
+
+    def custom_de_bend(x):
+        linear_part = x
+        inv_positive = torch.pow( x, 4 )
+        inv_negative = -torch.pow( -x, 4 )
+        return torch.where(x > 1, inv_positive, torch.where(x < -1, inv_negative, linear_part))
+
+    epsilon = torch.tensor(4e-8, dtype=torch.float32).to(image_array.device)
+    # clamp image befor arctanh
+    image_array = torch.clamp((image_array * 2) - 1, -1.0 + epsilon, 1.0 - epsilon)
+    # restore values from tanh  s-curve
+    image_array = torch.arctanh(image_array)
+    # restore custom bended values
+    image_array = custom_de_bend(image_array)
+    # move it to 0.0 - 1.0 range
+    image_array = ( image_array + 1.0) / 2.0
+
+    return image_array
+
 def main():
     parser = argparse.ArgumentParser(description='Training script.')
 
@@ -531,6 +566,22 @@ def main():
     else:
         print (f'Model type {args.type} is not yet implemented')
         sys.exit()
+
+    if args.model_path:
+        trained_model_path = args.model_path
+    else:
+        traned_model_name = 'flameSimpleML_model_' + fw.create_timestamp_uid()
+        trained_model_dir = os.path.join(
+            os.path.expanduser('~'),
+            'flameSimpleML_models')
+        os.makedirs(trained_model_dir)
+        trained_model_path = os.path.join(trained_model_dir, traned_model_name)
+
+    warmup_epochs = args.warmup
+    pulse_dive = args.pulse_amplitude
+    pulse_period = args.pulse
+    lr = args.lr
+    batch_size = 1
 
     criterion_mse = torch.nn.MSELoss()
     criterion_l1 = torch.nn.L1Loss()
