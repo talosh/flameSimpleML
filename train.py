64dc3fa203e6a5d9a7c0361b20d734c95498c47a
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-22 22:56:26 +0000

    update train.py

--- train.py
+++ train.py
@@ -667,7 +667,7 @@ def main():
 
             loss = criterion_mse(output, target)
             loss_l1 = criterion_l1(output, target)
-            loss_l1_str = str(f'{loss_l1.item():.4f}')
+            loss_l1_str = str(f'{loss_l1.item():.6f}')
 
             epoch_loss.append(float(loss_l1))
             steps_loss.append(float(loss_l1))
@@ -747,7 +747,7 @@ def main():
         hours = int((epoch_time % (24 * 3600)) // 3600)
         minutes = int((epoch_time % 3600) // 60)
         print (f'\r {" "*120}', end='')
-        print(f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Min: {min(epoch_loss):.4f} Avg: {smoothed_loss:.4f}, Max: {max(epoch_loss):.4f}')
+        print(f'\rEpoch [{epoch + 1} - {days:02}d {hours:02}:{minutes:02}], Min: {min(epoch_loss):.6f} Avg: {smoothed_loss:.6f}, Max: {max(epoch_loss):.6f}')
         steps_loss = []
         epoch_loss = []
         epoch = epoch + 1
