c0ebeb83ba43cfdd96eb6c329a3dd0f135bddea6
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-22 23:29:00 +0000

    update flameSimpleML.py and train.py

--- flameSimpleML.py
+++ flameSimpleML.py
@@ -144,6 +144,118 @@ class ApplyModelDialog(QtWidgets.QDialog):
         self.fw.save_prefs()
         self.pathLineEdit.setText(self.working_folder)
 
+
+class DatasetDialog(QtWidgets.QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        try:
+            self.fw = flameAppFramework()
+        except:
+            self.fw = None
+
+        self.dataset_folder = self.fw.prefs.get('dataset_folder', os.path.expanduser('~'))
+        if os.getenv('FLAMESMML_DATASET_FOLDER'):
+            self.dataset_folder = os.getenv('FLAMESMML_DATASET_FOLDER')
+        self.fw.prefs['dataset_folder'] = self.dataset_folder
+        self.fw.save_prefs()
+
+        self.setMinimumSize(480, 80)
+        self.setWindowTitle('flameSimpleML: Choose output folder:')
+
+        # Setting up layouts
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.buttonLayout = QtWidgets.QHBoxLayout()
+        self.pathLayout = QtWidgets.QHBoxLayout()
+
+        # Spacer
+        lbl_Spacer = QtWidgets.QLabel('', self)
+        lbl_Spacer.setStyleSheet('QFrame {color: #989898; background-color: #313131}')
+        lbl_Spacer.setMinimumHeight(4)
+        lbl_Spacer.setMaximumHeight(4)
+        lbl_Spacer.setAlignment(QtCore.Qt.AlignCenter)
+
+        lbl_WorkFolder = QtWidgets.QLabel('Export folder', self)
+        lbl_WorkFolder.setStyleSheet('QFrame {color: #989898; background-color: #373737}')
+        lbl_WorkFolder.setMinimumHeight(28)
+        lbl_WorkFolder.setMaximumHeight(28)
+        lbl_WorkFolder.setAlignment(QtCore.Qt.AlignCenter)
+        self.pathLayout.addWidget(lbl_WorkFolder)
+
+        def txt_WorkFolder_textChanged():
+            self.dataset_folder = txt_WorkFolder.text()
+
+        if os.getenv('FLAMETWML_WORK_FOLDER'):
+            lbl_WorkFolderPath = QtWidgets.QLabel(self.dataset_folder, self)
+            lbl_WorkFolderPath.setStyleSheet('QFrame {color: #989898; background-color: #373737}')
+            lbl_WorkFolderPath.setMinimumHeight(28)
+            lbl_WorkFolderPath.setMaximumHeight(28)
+            lbl_WorkFolderPath.setAlignment(QtCore.Qt.AlignCenter)
+            self.pathLayout.addWidget(lbl_WorkFolderPath)
+
+        else:
+            # Work Folder Text Field
+            hbox_workfolder = QtWidgets.QHBoxLayout()
+            hbox_workfolder.setAlignment(QtCore.Qt.AlignLeft)
+
+            txt_WorkFolder = QtWidgets.QLineEdit('', self)
+            txt_WorkFolder.setFocusPolicy(QtCore.Qt.ClickFocus)
+            txt_WorkFolder.setMinimumSize(280, 28)
+            txt_WorkFolder.setStyleSheet('QLineEdit {color: #9a9a9a; background-color: #373e47; border-top: 1px inset #black; border-bottom: 1px inset #545454}')
+            txt_WorkFolder.setText(self.dataset_folder)
+            txt_WorkFolder.textChanged.connect(txt_WorkFolder_textChanged)
+            hbox_workfolder.addWidget(txt_WorkFolder)
+
+            btn_changePreset = QtWidgets.QPushButton('Choose', self)
+            btn_changePreset.setFocusPolicy(QtCore.Qt.NoFocus)
+            btn_changePreset.setMinimumSize(88, 28)
+            btn_changePreset.setStyleSheet('QPushButton {color: #9a9a9a; background-color: #424142; border-top: 1px inset #555555; border-bottom: 1px inset black}'
+                                    'QPushButton:pressed {font:italic; color: #d9d9d9}')
+            btn_changePreset.clicked.connect(self.chooseFolder)
+            hbox_workfolder.addWidget(btn_changePreset, alignment = QtCore.Qt.AlignLeft)
+
+            self.pathLineEdit = txt_WorkFolder
+            self.pathLayout.addLayout(hbox_workfolder)
+
+        select_btn = QtWidgets.QPushButton('Export', self)
+        select_btn.setFocusPolicy(QtCore.Qt.NoFocus)
+        select_btn.setMinimumSize(128, 28)
+        select_btn.setStyleSheet('QPushButton {color: #9a9a9a; background-color: #424142; border-top: 1px inset #555555; border-bottom: 1px inset black}'
+                                'QPushButton:pressed {font:italic; color: #d9d9d9}')
+        select_btn.clicked.connect(self.accept)
+        select_btn.setAutoDefault(True)
+        select_btn.setDefault(True)
+
+        cancel_btn = QtWidgets.QPushButton('Cancel', self)
+        cancel_btn.setFocusPolicy(QtCore.Qt.NoFocus)
+        cancel_btn.setMinimumSize(128, 28)
+        cancel_btn.setStyleSheet('QPushButton {color: #9a9a9a; background-color: #424142; border-top: 1px inset #555555; border-bottom: 1px inset black}'
+                                'QPushButton:pressed {font:italic; color: #d9d9d9}')
+        cancel_btn.clicked.connect(self.reject)
+
+        # Adding widgets to the button layout
+        self.buttonLayout.addWidget(cancel_btn)
+        self.buttonLayout.addWidget(select_btn)
+
+        # Adding layouts to the main layout
+        self.layout.addLayout(self.pathLayout)
+        self.layout.addWidget(lbl_Spacer)
+        self.layout.addLayout(self.buttonLayout)
+
+        self.setStyleSheet('background-color: #313131')
+        
+        # self.setWindowFlags(QtCore.Qt.Tool)
+        self.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+
+    def chooseFolder(self):
+        result_folder = str(QtWidgets.QFileDialog.getExistingDirectory(self, 'Open Folder', self.dataset_folder, QtWidgets.QFileDialog.ShowDirsOnly))
+        if result_folder =='':
+            return
+        self.dataset_folder = result_folder
+        self.fw.prefs['dataset_folder'] = self.dataset_folder
+        self.fw.save_prefs()
+        self.pathLineEdit.setText(self.dataset_folder)
+
 def get_media_panel_custom_ui_actions():
     def scope_clip(selection):
         try:
@@ -274,30 +386,34 @@ def get_media_panel_custom_ui_actions():
 
     def train_model(selection):
         import flame
-        flame_version = flame.get_version()
-        python_executable_path = f'/opt/Autodesk/python/{flame_version}/bin/python'
-        script_folder = os.path.abspath(os.path.dirname(__file__))
-        app_name = settings.get('app_name')
-        version = settings.get('version')
-        msg = f'GUI for model training is not yet implemented in {app_name} {version}\n'
-        msg += f'Training is currently possible with a command-line script.\n'
-        msg += f'Please run\n"{python_executable_path} {script_folder}/train.py --help"'
-        dialog = flame.messages.show_in_dialog(
-            title ='Train Model GUI is not yet implemented',
-            message = msg,
-            type = 'info',
-            buttons = ['Copy', 'Ok'])
-        if dialog == 'Copy':
-            try:
-                from PySide6.QtWidgets import QApplication
-            except ImportError:
-                from PySide2.QtWidgets import QApplication
 
-            app = QApplication.instance()
-            if not app:
-                app = QApplication(sys.argv)
-            clipboard = app.clipboard()
-            clipboard.setText(f'{python_executable_path} {script_folder}/train.py --help')
+        dataset_dialog = DatasetDialog()
+
+        if dataset_dialog.exec():
+            flame_version = flame.get_version()
+            python_executable_path = f'/opt/Autodesk/python/{flame_version}/bin/python'
+            script_folder = os.path.abspath(os.path.dirname(__file__))
+            app_name = settings.get('app_name')
+            version = settings.get('version')
+            msg = f'GUI for model training is not yet implemented in {app_name} {version}\n'
+            msg += f'Training is currently possible with a command-line script.\n'
+            msg += f'Please run\n"{python_executable_path} {script_folder}/train.py --help"'
+            dialog = flame.messages.show_in_dialog(
+                title ='Train Model GUI is not yet implemented',
+                message = msg,
+                type = 'info',
+                buttons = ['Copy', 'Ok'])
+            if dialog == 'Copy':
+                try:
+                    from PySide6.QtWidgets import QApplication
+                except ImportError:
+                    from PySide2.QtWidgets import QApplication
+
+                app = QApplication.instance()
+                if not app:
+                    app = QApplication(sys.argv)
+                clipboard = app.clipboard()
+                clipboard.setText(f'{python_executable_path} {script_folder}/train.py --help')
 
     def about_dialog():
         pass
