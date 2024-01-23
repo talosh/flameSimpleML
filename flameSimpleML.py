5702ce8f88d4b18182c987ba277ea3ca3aaea379
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-23 00:06:25 +0000

    update flameSimpleML.py

--- flameSimpleML.py
+++ flameSimpleML.py
@@ -436,9 +436,11 @@ def get_media_panel_custom_ui_actions():
             script_folder = os.path.abspath(os.path.dirname(__file__))
             app_name = settings.get('app_name')
             version = settings.get('version')
+            command = f'{python_executable_path} {script_folder}/train.py {dataset_folder}'
             msg = f'GUI for model training is not yet implemented in {app_name} {version}\n'
             msg += f'Training is currently possible with a command-line script.\n'
-            msg += f'Please run\n"{python_executable_path} {script_folder}/train.py {dataset_folder}"'
+            msg += f'Please run\n{command}'
+            msg += '\n use --help for more options'
             dialog = flame.messages.show_in_dialog(
                 title ='Train Model GUI is not yet implemented',
                 message = msg,
@@ -454,7 +456,7 @@ def get_media_panel_custom_ui_actions():
                 if not app:
                     app = QApplication(sys.argv)
                 clipboard = app.clipboard()
-                clipboard.setText(f'{python_executable_path} {script_folder}/train.py --help')
+                clipboard.setText(command)
 
     def about_dialog():
         pass
