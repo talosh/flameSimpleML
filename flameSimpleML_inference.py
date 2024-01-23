8b222846ea6e24e667e69658c255b3e22da964b4
Andriy Toloshny <andriy.toloshnyy@gmail.com>
2024-01-23 02:17:40 +0000

    update flameSimpleML_inference.py and multires_v001.py

--- flameSimpleML_inference.py
+++ flameSimpleML_inference.py
@@ -2105,10 +2105,7 @@ class flameSimpleMLInference(QtWidgets.QWidget):
             src_image_data = src_image_data.to(self.torch_device, dtype=torch.float32)
         else:
             src_image_data = src_image_data.to(self.torch_device, dtype=torch.half)
-
-        pprint (platform.system())
-        print (f'dtype: {src_image_data.dtype}')
-
+            
         time_stamp = time.time()
         
         with torch.no_grad():
